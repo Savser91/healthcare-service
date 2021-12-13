6864467e8db8b98300119ce40b2f07cd32c41689
Savser91 <savser91@gmail.com>
2021-12-13 21:54:38 +0300

    first working test

--- src/test/java/service/medical/TestMedicalServiceImpl.java
+++ src/test/java/service/medical/TestMedicalServiceImpl.java
@@ -1,29 +1,35 @@
 package service.medical;
-
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.*;
 import ru.netology.patient.repository.PatientInfoFileRepository;
 import ru.netology.patient.service.alert.SendAlertServiceImpl;
-import ru.netology.patient.service.medical.MedicalServiceImpl;
+import ru.netology.patient.service.medical.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class TestMedicalServiceImpl {
-    PatientInfoFileRepository patientInfoFileRepository;
-    SendAlertServiceImpl service;
-    MedicalServiceImpl medicalService;
     String patientId = "id";
     PatientInfo patientInfo = new PatientInfo(patientId,"Ivan",
             "Ivanov", LocalDate.of(1990, 5, 10),
             new HealthInfo(new BigDecimal("36.6"),
                     new BloodPressure(110, 65)));
-    @BeforeEach
-    public void setMock() {
-        patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
-        service = Mockito.mock(SendAlertServiceImpl.class);
-        medicalService = new MedicalServiceImpl(patientInfoFileRepository, service);
+
+    @Test
+    public void check_GetWarningIfBloodPressureIsNotOk(){
+        PatientInfoFileRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        SendAlertServiceImpl service = Mockito.mock(SendAlertServiceImpl.class);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoFileRepository, service);
+        Mockito.when(patientInfoFileRepository.getById(patientId))
+                .thenReturn(patientInfo);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        String expected = String.format("Warning, patient with id: %s, need help", patientId);
+        medicalService.checkBloodPressure(patientId, new BloodPressure(130, 90));
+        Mockito.verify(service, Mockito.only()).send(captor.capture());
+        Assertions.assertEquals(expected, captor.getValue());
     }
 
 }
